c8c41657d9ef47d942394304bc676de1fd68e7be
RHAYMISON <rhaymison@hotmail.com>
2021-10-01 14:41:35 -0300

    createblock

--- blockchain.py
+++ blockchain.py
@@ -8,7 +8,20 @@ from flask import Flask, jsonify
 
 class BlockChain:
     def __init__(self):
-        
+
         self.chain = []
 
         self.create_block(proof=1, previous_hash='0')
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'previous_hash': previous_hash,
+        }
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
